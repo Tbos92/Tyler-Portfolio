884e5830a76461ae15093a6864c942584bdaaa9a
Tyler <tbos92@gmail.com>
2024-08-13 15:05:08 -0400

    Pushing all changes made for feature-contact as well as related styling upgrades that occurred along with new feature

--- javascript.js
+++ javascript.js
@@ -1,1 +1,28 @@
+const form = document.querySelector("#form");
+const submitButton = document.querySelector("#submit");
+
+form.addEventListener("submit", (e) => {
+  submitButton.disabled = true;
+  e.preventDefault();
+  window.location.href = window.location.origin + "/success.html";
+});
 console.log("Hello World!");
+// For sticky navbar: when the user scrolls the page, execute myFunction
+window.onscroll = function () {
+  myFunction;
+};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// add the sticky class to the navbar when you reach scroll position. Remove sticky when you leave scroll position
+function myFunction() {
+  if (window.scrollY >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
